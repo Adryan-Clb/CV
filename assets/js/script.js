c506df85bc12a2ef7062d6450053d32c24accf61
Adryan-Clb <adryan.clmb@gmail.com>
2023-10-31 15:37:25 +0100

    remove

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,31 +15,50 @@ const icon = [
     {
         name: 'HTML',
         logo: 'html-5',
+        link: 'https://fr.wikipedia.org/wiki/Hypertext_Markup_Language',
     },
     {
         name: 'CSS',
-        logo: 'css3'
+        logo: 'css3',
+        link: 'https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade',
     },
     {
-        name: 'Bootstrap',
+        name: 'Boot strap',
         logo: 'bootstrap',
+        link: 'https://fr.wikipedia.org/wiki/Bootstrap_(framework)',
     },
     {
-        name: 'WordPress',
+        name: 'Word Press',
         logo: 'wordpress',
+        link: 'https://fr.wikipedia.org/wiki/WordPress',
     },
     {
-        name: 'JS',
-        logo: 'javascript'
+        name: 'Java Script',
+        logo: 'javascript',
+        link: 'https://fr.wikipedia.org/wiki/JavaScript'
+    },
+    {
+        name: 'My SQL',
+        logo: 'mysql',
+        link: 'https://fr.wikipedia.org/wiki/MySQL',
     }
 ];
 const AffIcon = document.querySelector('.listicon');
 
 for (i = 0; i < icon.length; i++) {
-const textHtml = document.createElement('div')
-AffIcon.appendChild(textHtml)
+    const textHtml = document.createElement('div');
+    AffIcon.appendChild(textHtml).classList.add('test');
+    
+    const imgHtml = document.createElement('img')
+    imgHtml.src = `assets/img/logo/${icon[i].logo}.png`
+    textHtml.appendChild(imgHtml).classList.add('img');
+        const textest = document.createElement('a')
+        textest.href = icon[i].link
+        textest.textContent = icon[i].name
+        textHtml.appendChild(textest).classList.add('auto');
 
-const imgHtml = document.createElement('img')
-imgHtml.src = `assets/img/logo/${icon[i].logo}.png`
-textHtml.appendChild(imgHtml)
-}
+        if (textest.textContent.length < 4){
+            textest.style.fontSize = '16px'
+        }
+    }
+   
